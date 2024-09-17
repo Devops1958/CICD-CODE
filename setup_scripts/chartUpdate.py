cb0c1ab826a1337c74a7a380f66676e6375c6195
unknown <nyoyo@gmail.com>
2024-09-16 20:25:00 -0400

    Jenkinsfile

--- setup_scripts/chartUpdate.py
+++ setup_scripts/chartUpdate.py
@@ -2,22 +2,45 @@ import shutil
 import sys
 
 
-APP_VERSION = sys.arg[1]
+def checkversionArgument():
+    if sys.argv[1]:
+        return sys.argv[1]
+    else:
+        print("Missing Release version")
+        sys.exit(98) 
+def takeBackup():
 
-shutil.copy('geoapp/Chart.yaml', 'geoapp/chart_bk.yaml')
+    try:        
+
+       shutil.copy('geoapp/Chart.yaml', 'geoapp/chart_bk.yaml')
+    except Exception as e:
+        print(f"Error: {e}")   
 
 def chartModif(chart_version):
-    with open("projects/Chart.yaml", 'r') as f:
-        content = f.readlines()
+    try: 
+      with open("geoapp/Chart.yaml", 'r') as f:
+         content = f.readlines()
+    except Exception as e:
+        print(f"Error: {e}") 
+        sys.exit(99)
+    else:       
         
-    with open("projects/Chart.yaml" , 'w')  as f1:
+       with open("geoapp/Chart.yaml" , 'w')  as f1:
         for line in content:
             if 'version:' in line:
                 f1.write(f'version: {chart_version}\n')
             elif 'appVersion:' in line: 
-                f1.write(f'version: {chart_version}\n') 
+                f1.write(f'appversion: {chart_version}\n') 
             else:
                 f1.write(line)
+        print(f"Chart updated to version {chart_version}")  
+def main():
+    App_VERSION=checkversionArgument()
+    takeBackup()
+    chartModif(App_VERSION)
+
+if __name__== "__main__":
+    main                  
      
-chartModif(APP_VERSION)    
+   
            
